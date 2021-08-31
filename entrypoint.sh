21f859943cdb515ad3e0fd7f4edae27984a3afa9
Marvin Gabler <51857438+marvingabler@users.noreply.github.com>
2021-08-31 22:42:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ else
     wget -N -O - https://download1.graphhopper.com/public/photon-db-latest.tar.bz2 | pbzip2 -cd | tar x
 fi
 
-ls
-
 if [ -f "photon-0.3.5.jar" ]; then
     echo "Found local photon version"
 else
